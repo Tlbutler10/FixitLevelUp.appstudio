d16bd0490e882123d3a1af054b174623fa54140c
Tanner <tannerbutler15@gmail.com>
2019-09-29 22:12:08 -0500

    Update Form1.js

--- forms/Form1/Form1.js
+++ forms/Form1/Form1.js
@@ -38,11 +38,10 @@ case 'Home':
   let amount = Number(prompt('How much will the house cost?'))
   let timeFrame = Number(prompt('How many years will you be paying on the house?'))
   let interest = Number(prompt('What will your interest rate be on the house?'))
-  alert(`A home loan for $${amosunt} over ${timeFrame} years at ${interest}% interest would have a monthly payment of ${homeLoanPayment(amount,timeFrame,interest)}.`)
+  alert(`A home loan for $${amount} over ${timeFrame} years at ${interest}% interest would have a monthly payment of ${homeLoanPayment(amount,timeFrame,interest)}.`)
   break;
-case 'STOP':
-    break;
 default:
   alert('Sorry that is not an option. Choose either Car or Home. If you would like to end this type STOP on the following prompt')
  }
+if (i=='STOP') break;
 }
